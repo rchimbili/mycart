06aff3a56cfd78adbd3cd9b70b6cda1b1a0aab2e
Ubuntu <ubuntu@ip-172-31-89-196.ec2.internal>
2019-09-09 11:54:55 +0000

    msmycart

--- frontend/code/index.php
+++ frontend/code/index.php
@@ -1,5 +1,5 @@
 <?php
-echo "<h1> Welcome to Docker</h1>";
+echo "<h1> This is the Three Tier Architecture Test</h1>";
 echo "<h3> Your Web Running Apache with php on Frontend container </h3>";
 echo "<h3> Your APP Running Apache and PHP providing API services for User information on url http://backend/get_user_details </h2>";
 echo "<h3> Your Database running Mysql/Mariadb holding user information in userdb on users Table </h1>";
@@ -12,7 +12,10 @@ curl_close($conn);
 return $response;
 }
 $backendlb = getenv("BACKENDLB");
-$backendurl="http://$backendlb/backend.php";
-$backendoutput=httpPost($backendurl);
-echo $backendoutput;
+$baseurl="http://$backendlb/bootstrap.php";
+$response=httpPost($baseurl);
+echo $response;
+$testurl="http://$backendlb/getusers.php";
+$candllabassoc_id=httpPost($testurl);
+echo $candllabassoc_id;
 ?>
